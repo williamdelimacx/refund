e5ffb10505db14fa0c307e90a5c15a3c989175fc
William de Lima <williamdelimacx@gmail.com>
2025-12-26 16:57:12 -0300

    feat: add input component

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,15 @@
+import { Input } from "../components/Input";
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <form className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+      />
+      <Input required legend="Senha" type="password" placeholder="123456" />
+    </form>
+  );
 }
