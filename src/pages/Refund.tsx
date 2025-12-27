a0e249acad2b4799f7a98d20e045cf5756365579
William de Lima <williamdelimacx@gmail.com>
2025-12-27 18:54:14 -0300

    feat: add upload component

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
+
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { Upload } from "../components/Upload";
 
 export function Refund() {
   const [category, setCategory] = useState("");
@@ -36,6 +38,8 @@ export function Refund() {
 
         <Input legend="Valor" required />
       </div>
+
+      <Upload filename="william.png" />
     </form>
   );
 }
