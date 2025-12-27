10b4aec35d47ef120f6afdba4d86b9d4becc3a8c
William de Lima <williamdelimacx@gmail.com>
2025-12-27 18:00:31 -0300

    feat: add select component

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,5 @@
 import { Input } from "../components/Input";
+import { Select } from "../components/Select";
 
 export function Refund() {
   return (
@@ -13,6 +14,7 @@ export function Refund() {
       </header>
 
       <Input required legend="Nome da solicitação" />
+      <Select required legend="Categoria" />
     </form>
   );
 }
