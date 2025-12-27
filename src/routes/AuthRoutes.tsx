145839e89649f828009909b1dcb3ebd076185437
William de Lima <williamdelimacx@gmail.com>
2025-12-27 17:25:19 -0300

    feat: add sign up

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 
 import { AuthLayout } from "../components/AuthLayout";
 import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AuthLayout />}>
         <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Route>
     </Routes>
   );
