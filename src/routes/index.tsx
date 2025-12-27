838e91810b4f8848153bed921f65750bf5e6fcec
William de Lima <williamdelimacx@gmail.com>
2025-12-27 20:52:47 -0300

    feat: add dashboard page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter } from "react-router";
 
-import { AuthRoutes } from "./AuthRoutes";
-import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <EmployeeRoutes />
+      <ManagerRoutes />
     </BrowserRouter>
   );
 }
