7d36a0098861ba37d001f78e0e6abe27c51f8f42
William de Lima <williamdelimacx@gmail.com>
2025-12-27 19:02:12 -0300

    feat: add send button

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -5,12 +5,26 @@ import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
 
 export function Refund() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [filename, setFilename] = useState<File | null>(null);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    console.log(name, amount, category, filename);
+  }
 
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form
+      onSubmit={onSubmit}
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]"
+    >
       <header>
         <h1 className="text-xl font-bold text-gray-100">
           Solicitação de reembolso
@@ -20,7 +34,12 @@ export function Refund() {
         </p>
       </header>
 
-      <Input required legend="Nome da solicitação" />
+      <Input
+        required
+        legend="Nome da solicitação"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <div className="flex gap-4">
         <Select
@@ -36,10 +55,22 @@ export function Refund() {
           ))}
         </Select>
 
-        <Input legend="Valor" required />
+        <Input
+          legend="Valor"
+          required
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </div>
 
-      <Upload filename="william.png" />
+      <Upload
+        filename={filename && filename.name}
+        onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Enviar
+      </Button>
     </form>
   );
 }
