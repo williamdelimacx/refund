d81070083d0765f547622430323422dea2071647
William de Lima <williamdelimacx@gmail.com>
2025-12-27 23:16:54 -0300

    feat: add user role

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,40 @@
+/* eslint-disable react-hooks/static-components */
 import { BrowserRouter } from "react-router";
 
 import { Loading } from "../components/Loading";
 
 import { AuthRoutes } from "./AuthRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
 
 const isLoading = false;
+// const session = undefined
+
+const session = {
+  user: {
+    role: "manager",
+  },
+};
 
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
   if (isLoading) {
     return <Loading />;
   }
 
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
